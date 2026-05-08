21866b1c3bbf352015597912e6812cb4f4f9d9f2
Aatika Hyder <aatikahyder27@gmail.com>
2026-05-08 14:06:34 -0400

    Added Google Analytics integration in React app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
 import "./App.css";
+import ReactGA from "react-ga4";
 
 function App() {
   const[task, setTask] = useState("");
@@ -13,22 +14,43 @@ function App() {
   const addTask = () => {
     setTasks([...tasks, {text: task, completed: false}]);
     setTask("");
-  }
+    ReactGA.event({
+      category: "Task",
+      action: "Added Task",
+    });
+  };
   const deleteTask = (index) => {
     setTasks(tasks.filter((_,i) => i !== index))
+    ReactGA.event({
+      category: "Task",
+      action: "Deleted Task",
+  });
   };
   const toggleTask = (index) => {
-    const updatedTasks = tasks.map((t,i) => {
-        if (i === index) {
-          return {...t, completed: !t.completed};
-        }
-        return t;
+    const updatedTasks = tasks.map((t, i) => {
+      if (i === index) {
+        const updated = { ...t, completed: !t.completed };
+
+        ReactGA.event({
+          category: "Task",
+          action: updated.completed ? "Completed Task" : "Uncompleted Task",
+        });
+
+        return updated;
+      }
+      return t;
     });
+
     setTasks(updatedTasks);
   };
+
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+  useEffect(() => {
+    ReactGA.initialize("G-ER0MT737N4");
+    ReactGA.send("pageview");
+  }, []);
 
   return(
     <div className="container">
